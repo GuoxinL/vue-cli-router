fd770b6016c7b0d819ec182404c976cf40bf0986
Guoxin.Liu <lgx31@sina.cn>
2017-08-13 00:13:37 +0800

    Forget where you learned

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">    <br/>
+    <img src="./assets/logo.png"> <br/>
     <router-link to="/">Home</router-link>
     <router-link to="/news/123/321">News</router-link>
 
-    <router-view></router-view>
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -22,5 +24,26 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+/*
+组件过渡过程中，会有四个CSS类名进行切换，这四个类名与transition的name属性有关，比如name=”fade”,会有如下四个CSS类名：
+
+fade-enter:进入过渡的开始状态，元素被插入时生效，只应用一帧后立刻删除。
+fade-enter-active:进入过渡的结束状态，元素被插入时就生效，在过渡过程完成后移除。
+fade-leave:离开过渡的开始状态，元素被删除时触发，只应用一帧后立刻删除。
+fade-leave-active:离开过渡的结束状态，元素被删除时生效，离开过渡完成后被删除。
+ */
+  .fade-enter {
+    opacity:0;
+  }
+  .fade-leave{
+    opacity:1;
+  }
+  .fade-enter-active{
+    transition:opacity .5s;
+  }
+  .fade-leave-active{
+    opacity:0;
+    transition:opacity .5s;
   }
 </style>
--- src/components/News.vue
+++ src/components/News.vue
@@ -3,16 +3,16 @@
 */
 
 <template>
-    <h2>{{ msg }}</h2>
+  <h2>{{ msg }}</h2>
 </template>
 <script>
-    export default{
-        data(){
-            return {
-                msg: 'I am News'
-            }
-        }
+  export default{
+    data(){
+      return {
+        msg: 'I am News'
+      }
     }
+  }
 </script>
 <style>
 
